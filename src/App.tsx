101293c49bc47af9d44084adacf7f85b9e7670c2
tozxart <fallag.rayen@gmail.com>
2024-12-02 23:04:24 +0100

    Update router and vite config for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { MainPage } from "./pages/MainPage";
 import { AboutPage } from "./pages/AboutPage";
